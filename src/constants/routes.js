571f1d6f0e7a73e931c9c0bbd509055b06c0a266
Maxwell Nelson <m.nelson.study@gmail.com>
2019-05-29 12:25:06 -0700

    Refactor routes into shared file between client and server side stores

---
+++ src/constants/routes.js
@@ -0,0 +1,10 @@
+export default {
+  ROOT: "/",
+  HOME: "/home",
+  DEMO: "/demo",
+  TRACKING: "/solutions/:category",
+  DELIVERY: "/solutions/:category",
+  INVENTORY: "/solutions/:category",
+  RETAIL: "/solutions/:category",
+  HOW_IT_WORKS: "/howitworks"
+};
