a8d69e7c9e7594b13a80a668e0d658c10a29cf6b
pfeinman <peter.feinman@gmail.com>
2019-07-18 15:54:56 -0700

    Adds CartMenu and CartMenuButton components

---
+++ src/components/NavBar/CartMenuButton.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import styled from "styled-components";
+import { toggleCartMenu } from "../../actions/ui";
+
+// import './HamburgerMenuButton.styl'
+// We use animated-hamburger.scss because we can't use styled-components :(
+
+const Bar = styled.div`
+  width: 20px;
+  height: 1px;
+  background-color: #718ba8;
+  margin: 5px 0;
+  transition: 0.4s;
+`;
+
+const Container = styled.div`
+  display: inline-block;
+  cursor: pointer;
+  outline: none;
+`;
+
+const CartMenuButton = ({ _toggleCartMenu }) => (
+  <Container
+    tabIndex="0"
+    role="button"
+    onKeyPress={() => {}}
+    onClick={() => {
+      _toggleCartMenu();
+    }}
+  >
+    {/* <Bar />
+    <Bar />
+    <Bar /> */}
+    <button>Cart</button>
+  </Container>
+);
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      _toggleCartMenu: toggleCartMenu
+    },
+    dispatch
+  );
+export default connect(
+  null,
+  mapDispatchToProps
+)(CartMenuButton);
