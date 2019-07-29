ad5ca3c6547c7f8dc8d906501b52340765b3bfaf
Nilson <nilson.ivano@gmail.com>
2019-07-29 16:41:47 -0300

    New product page scaffold

--- src/components/singleProduct/index.js
+++ src/components/singleProduct/index.js
@@ -15,216 +15,79 @@ type PropsType = {
   products: ProductType[]
 };
 
-const ProductPageContainer = styled.div`
-  display: block;
-`;
-
-const OrderContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-`;
-
-const ProductInfo = styled.div`
-  margin: 5%;
-`;
-
-const PriceLabel = styled.span`
-  color: #29ace4 !important;
-`;
-
-const QuantityContainer = styled.div`
-  display: grid;
-  grid-template-rows: 1fr 1fr;
-  margin: 0 auto;
-  width: 50%;
-`;
-
-const QuantityControls = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const QuantityButton = styled.button`
-  width: 20%;
-`;
-
-const OrderSubmitButton = styled.button`
-  width: 100%;
-  margin-top: 5%;
-`;
-
-const ProductImage = styled.img`
-  min-height: 400px;
-  min-width: 400px;
-  margin: 0 auto;
-`;
-
-const ActionContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin: 0 auto;
-  justify-content: space-evenly;
-`;
-
-const ProductAction = styled.div`
-  font-size: 0.5em;
-  text-transform: uppercase;
-  cursor: pointer;
-  display: flex;
-  flex-direction: column;
-  margin: 5%;
-  text-align: center;
-  align-items: center;
-`;
-
-const Heart = styled.div`
-  background-image: url(https://enchanteddiamonds.com/assets/icons-031cc6b9b1ccef9a9ef955b1e9de07331b2470d73010a61a900cb93585ed1d5c.png);
-  background-size: 500% 2000%;
-  background-position: 50% 15.7894736842%;
-  max-width: 32px !important;
-  max-height: 32px !important;
-  display: inline-block;
-  min-width: 32px;
-  min-height: 32px;
-`;
-const Share = styled.div`
-  background-image: url(https://enchanteddiamonds.com/assets/icons-031cc6b9b1ccef9a9ef955b1e9de07331b2470d73010a61a900cb93585ed1d5c.png);
-  background-size: 500% 2000%;
-  background-position: 100% 21.0526315789%;
-  max-width: 32px !important;
-  max-height: 32px !important;
-  display: inline-block;
-  min-width: 32px;
-  min-height: 32px;
-`;
-const Chat = styled.div`
-  background-image: url(https://enchanteddiamonds.com/assets/icons-031cc6b9b1ccef9a9ef955b1e9de07331b2470d73010a61a900cb93585ed1d5c.png);
-  background-size: 500% 2000%;
-  background-position: 100% 31.5789473684%;
-  max-width: 32px !important;
-  max-height: 32px !important;
-  display: inline-block;
-  min-width: 32px;
-  min-height: 32px;
-`;
-
-const RelatedContainer = styled.div`
-  width: 100%;
-  flex: 4;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-evenly;
-  max-width: 1024px;
-  justify-self: center;
-  margin: 0 auto;
-`;
-
-const RelatedLabel = styled.span`
-  margin-left: 10%;
-`;
-
-const RelatedProduct = styled.div`
-  flex: 1;
-`;
-
-// todo
-const ProductLink = styled(Link)`
-  text-decoration: none;
-`;
-
-const ProductThumbnail = styled.img`
-  margin: 5%;
-  max-height: 100px;
-  max-width: 100%;
-`;
-
 // eslint-disable-next-line react/prop-types
 class SingleProduct extends React.Component<PropsType, StateType> {
-  render(): React.Node {
+  state = {
+    product: {
+      name: "650ml Spectrum CBD oil (Full Spectrum) Hemp Extract (60mg / ml)",
+      subtitle: "650ml Spectrum CBD oil (Full Spectrum) Hemp Extract (60mg / ml)",
+      description: "650ml Spectrum CBD oil (Full Spectrum) Hemp Extract (60mg / ml)",
+      price: 15,
+    }
+  };
+
+  render() {
     const { singleProduct = {}, _updateProductQuantity, products } = this.props;
+    const { product } = this.state;
     const subtotal =
       parseFloat(singleProduct.price) * parseFloat(singleProduct.quantity);
     return (
-      <ProductPageContainer>
-        <OrderContainer>
-          <ProductImage src={singleProduct.image} />
-
-          <ProductInfo>
-            <h1>{singleProduct.title}</h1>
-            <h3>{singleProduct.subtitle}</h3>
-            <p>{singleProduct.description}</p>
-
-            <PriceLabel>
-              <strong>${singleProduct.price}</strong>
-            </PriceLabel>
-
-            <ActionContainer>
-              <ProductAction>
-                <Heart />
-                Add To
-                <br />
-                Favorites
-              </ProductAction>
-
-              <ProductAction>
-                <Share />
-                Share
-              </ProductAction>
-
-              <ProductAction>
-                <Chat />
-                Chat With
-                <br />
-                An Expert
-              </ProductAction>
-            </ActionContainer>
-
-            <span>QTY</span>
-            <QuantityContainer>
-              <QuantityControls>
-                <QuantityButton
-                  onClick={() => _updateProductQuantity(singleProduct.id, -1)}
-                >
-                  -
-                </QuantityButton>
-                <h3>{singleProduct.quantity}</h3>
-                <QuantityButton
-                  onClick={() => _updateProductQuantity(singleProduct.id, +1)}
-                >
-                  +
-                </QuantityButton>
-              </QuantityControls>
-              <OrderSubmitButton>
-                ${subtotal.toFixed(2)} Add To Cart
-              </OrderSubmitButton>
-            </QuantityContainer>
-          </ProductInfo>
-        </OrderContainer>
-        <section>
-          <h2>
-            <RelatedLabel>Related</RelatedLabel>
-          </h2>
-          <RelatedContainer>
-            {products.map((product, i) => {
-              return (
-                <div key={product.id}>
-                  <ProductLink
-                    to={{
-                      type: "SINGLE_PRODUCT",
-                      payload: { productId: product.id }
-                    }}
-                  >
-                    <h4>{product.title}</h4>
-                    <ProductThumbnail src={product.image} />
-                  </ProductLink>
+      <section className="section content">
+        <div className="container is-fluid">
+          <div className="columns">
+            <div className="column is-6">
+              <figure className="image is-square">
+                <img src="https://bulma.io/images/placeholders/480x480.png"></img>
+              </figure>
+            </div>
+            <div className="column is-6">
+              <div className="content">
+                <h1>{ product.name }</h1>
+                <h3>{ product.subtitle }</h3>
+                <p>{ product.description }</p>
+              </div>
+              <h1>${ product.price.toFixed(2) }</h1>
+              <div className="columns is-centered m-t-md">
+                <div className="column">
+                  <div className='has-text-centered'>
+                    <span className="icon"><i className="far fa-lg fa-heart"></i></span>
+                  </div>
+                  <div className='has-text-centered'>Add to favorites</div>
                 </div>
-              );
-            })}
-          </RelatedContainer>
-        </section>
-      </ProductPageContainer>
+                <div className="column">
+                  <div className='has-text-centered'>
+                    <span className="icon is-medium"><i className="far fa-lg fa-share-square"></i></span>
+                  </div>
+                  <div className='has-text-centered'>Share</div>
+                </div>
+                <div className="column">
+                  <div className='has-text-centered'>
+                    <span className="icon"><i className="fas fa-lg fa-phone"></i></span>
+                  </div>
+                  <div className='has-text-centered'>Chat with an expert</div>
+                </div>
+              </div>
+              <div className="level">
+                <div className="level-item">
+                  <div className="button is-large is-primary m-t-lg">-</div>
+                </div>
+                <div className="level-item has-text-centered">
+                  <div>
+                    <p className="heading">Quantity</p>
+                    <p className="title">150</p>
+                  </div>
+                </div>
+                <div className="level-item">
+                  <div className="button is-large is-primary m-t-lg">+</div>
+                </div>
+              </div>
+              <div className="has-text-centered">
+                <div className="button is-large is-primary m-t-lg">Add to cart</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
