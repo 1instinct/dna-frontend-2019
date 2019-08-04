505ce911508afc838fa3c0828754c699c3429555
pfeinman <peter.feinman@gmail.com>
2019-08-04 15:27:29 -0700

    Adds Cart reducer to rootReducer exports (index)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,14 @@ import auth from "./auth";
 import page from "./page";
 import title from "./title";
 import ui from "./ui";
-
 import products from "./products";
+import cart from "./cart";
 
 export default {
   auth,
   page,
   title,
   products,
-  ui
+  ui,
+  cart
 };
